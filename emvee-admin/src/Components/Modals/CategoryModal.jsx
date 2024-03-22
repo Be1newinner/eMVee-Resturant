5e7e1259a5683ba48754d3b78f346795db6df6f2
Vijay Kumar <be1newinner@gmail.com>
2024-03-22 17:27:47 +0530

    added upload category thing

---
+++ emvee-admin/src/Components/Modals/CategoryModal.jsx
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CheckBox,
+  Input,
+  Modal,
+  Text,
+} from "@ui-kitten/components";
+import { Dimensions, Image, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { fireStorage } from "../../Infrastructure/firebase.config";
+import { ref, uploadBytes } from "firebase/storage";
+import * as ImageManipulator from "expo-image-manipulator";
+
+export const CategoryModal = ({ visible, setVisible }) => {
+  const [Name, setName] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [Error, setError] = useState("");
+  const [image, setImage] = useState(null);
+
+  async function uploadImageAsync({ uri, categoryID }) {
+    try {
+      const convertedBlob = await ImageManipulator.manipulateAsync(
+        uri,
+        [{ resize: { width: 500, height: 500 } }],
+        { compress: 1, format: ImageManipulator.SaveFormat.PNG } // Adjust format if needed
+      );
+
+      const response = await fetch(convertedBlob.uri);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch image");
+      }
+
+      const blob = await response.blob();
+      const fileRef = ref(fireStorage, "ca", categoryID, 1);
+      await uploadBytes(fileRef, blob);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const addNewCategory = () => {};
+
+  async function addCategory() {
+    setName("");
+    setChecked(false);
+    setError("");
+    setImage(null);
+    setVisible(false);
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      backdropStyle={{
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+      }}
+      onBackdropPress={() => {
+        setVisible(false);
+      }}
+    >
+      <Card
+        style={{
+          width: Dimensions.get("screen").width,
+          gap: 10,
+        }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 5,
+            marginBottom: 25,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 700,
+            }}
+          >
+            Add New Category
+          </Text>
+        </View>
+
+        <View>
+          {Error?.other && (
+            <Text
+              status="danger"
+              style={{
+                fontWeight: 700,
+              }}
+            >
+              {Error?.other}
+            </Text>
+          )}
+
+          <Input
+            value={Name}
+            label="Category Name"
+            placeholder="enter category name"
+            // accessoryLeft={() => <Entypo name="email" size={24} color="black" />}
+            status={Error?.Name ? "danger" : "basic"}
+            caption={
+              Error?.Name &&
+              (() => (
+                <Text
+                  status="danger"
+                  style={{
+                    fontSize: 13,
+                    fontWeight: 700,
+                    marginTop: 5,
+                  }}
+                >
+                  {Error?.Name}
+                </Text>
+              ))
+            }
+            onChangeText={(nextValue) => setName(nextValue)}
+            size="large"
+            style={{
+              elevation: 5,
+            }}
+          />
+          <CheckBox
+            checked={checked}
+            onChange={(nextChecked) => setChecked(nextChecked)}
+            style={{
+              marginTop: 20,
+            }}
+            status="danger"
+          >
+            Mark as Popular Category
+          </CheckBox>
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{
+                width: 200,
+                height: 200,
+                objectFit: "contain",
+                marginTop: 10,
+              }}
+            />
+          )}
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <Button
+              status="danger"
+              size="small"
+              style={{
+                maxWidth: 100,
+                marginTop: 10,
+              }}
+              onPress={pickImage}
+            >
+              Pick Image
+            </Button>
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            flex: 1,
+            width: "auto",
+            marginTop: 20,
+          }}
+        >
+          <Button
+            status="basic"
+            onPress={() => setVisible(false)}
+            style={{
+              flex: 1,
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onPress={() => {
+              addCategory();
+            }}
+            status="danger"
+            style={{
+              flex: 1,
+            }}
+          >
+            Add Category
+          </Button>
+        </View>
+      </Card>
+    </Modal>
+  );
+};
--- emvee-admin/src/Screens/BottomTabs/CategoriesScreen/index.js
+++ emvee-admin/src/Screens/BottomTabs/CategoriesScreen/index.js
@@ -1,87 +1,135 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { GlobalColors } from "../../../Infrastructure/GlobalVariables";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { AntDesign } from "@expo/vector-icons";
+import { CategoryModal } from "../../../Components/Modals/CategoryModal";
 
 export default function CategoriesScreen() {
   const [TotalCategories, setTotalCategories] = useState(0);
+  const categorySelector = useSelector((selector) => selector.AllCategories);
+
+  const [modalVisible, setModalVisible] = useState(true);
+
+  // useEffect(() => {
+  //   console.log(categorySelector);
+  // }, []);
+
   return (
     <View
       style={{
         backgroundColor: GlobalColors.primary,
         flex: 1,
-        padding: 10,
       }}
     >
       <View
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
-          marginVertical: 20,
-          marginLeft: 10,
-          marginRight: 20,
+          backgroundColor: GlobalColors.themeColor,
+          padding: 20,
+          elevation: 5,
+          alignItems: "center",
         }}
       >
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: 500,
-          }}
-        >
-          Total Categories
-        </Text>
         <Text
           style={{
             fontSize: 18,
             fontWeight: 700,
+            color: "#fff",
           }}
         >
-          {TotalCategories}
-        </Text>
-      </View>
-
-      <View
-        style={{
-          gap: 10,
-        }}
-      >
-        {[2, 5, 9, 3, 1, 8].map((item, index) => (
-          <View
-            key={item}
+          Total Categories
+          <Text
             style={{
-              backgroundColor: "#fff",
-              padding: 20,
-              flexDirection: "row",
-              gap: 10,
-              elevation: 3,
-              borderRadius: 10,
+              fontSize: 20,
+              fontWeight: 700,
+              color: "#fff",
             }}
           >
-            <Text
-              style={{
-                fontWeight: 500,
-              }}
-            >
-              {index}.
-            </Text>
-            <Text
-              style={{
-                fontWeight: 500,
-                flex: 1,
-              }}
-            >
-              Burgers
-            </Text>
-            <Text
-              style={{
-                fontWeight: 700,
-                fontSize: 16,
-              }}
-            >
-              ({item})
-            </Text>
-          </View>
-        ))}
+            {" "}
+            ({TotalCategories})
+          </Text>
+        </Text>
+
+        <AntDesign
+          name="pluscircle"
+          size={28}
+          color="#fff"
+          onPress={() => setModalVisible(true)}
+        />
       </View>
+
+      <FlatList
+        contentContainerStyle={{
+          gap: 10,
+          padding: 10,
+        }}
+        ListHeaderComponent={
+          <View
+            style={{
+              marginTop: 10,
+            }}
+          />
+        }
+        ListFooterComponent={
+          <View
+            style={{
+              marginBottom: 80,
+            }}
+          />
+        }
+        data={categorySelector}
+        keyExtractor={(k) => k.k}
+        renderItem={({ item, index }) => {
+          // console.log(item);
+          return (
+            <View
+              key={item.k}
+              style={{
+                backgroundColor: "#fff",
+                padding: 20,
+                flexDirection: "row",
+                gap: 10,
+                elevation: 3,
+                borderRadius: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: 500,
+                }}
+              >
+                {index + 1}.
+              </Text>
+              <Text
+                style={{
+                  fontWeight: 500,
+                  flex: 1,
+                }}
+              >
+                {item?.t}
+              </Text>
+              {item?.s && (
+                <AntDesign
+                  name="star"
+                  size={24}
+                  color={GlobalColors.themeColor}
+                />
+              )}
+              <Text
+                style={{
+                  fontWeight: 700,
+                  fontSize: 16,
+                }}
+              >
+                ({5})
+              </Text>
+            </View>
+          );
+        }}
+      />
+      <CategoryModal visible={modalVisible} setVisible={setModalVisible} />
     </View>
   );
 }
