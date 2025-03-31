4e77615276bbd81f8497e10b078992fd4da1b7fc
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-31 16:00:02 +0530

    clean up

--- emvee_main2/services/ContextProviders/index.js
+++
@@ -1,5 +0,0 @@
-import { StoreDetailsProvider } from "../StoreDetails/StoreDetailsContext";
-
-export default function ContextProviders({ children }) {
-  return <StoreDetailsProvider>{children}</StoreDetailsProvider>;
-}
